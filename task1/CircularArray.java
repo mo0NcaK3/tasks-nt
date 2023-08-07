dec15ad66eaa840fc12d0dbd0cb41d56ba628e0b
mo0NcaK3 <nadya.pavlienko@gmail.com>
2023-08-07 14:29:02 +0300

    fix: replace the setAll() method with a for loop

--- task1/CircularArray.java
+++ task1/CircularArray.java
@@ -1,13 +1,18 @@
 package task1;
-import java.util.Arrays;
+
 
 public class CircularArray {
     public static void main(String[] args) {
+        if (args.length < 2) {
+            return;
+        }
         int n = Integer.parseInt(args[0]);
         int m = Integer.parseInt(args[1]);
 
         int[] arr = new int[n];
-        Arrays.setAll(arr, i -> ++i);
+        for (int i = 1; i <= n; i++) {
+            arr[i - 1] = i;
+        }
 
         int current = 0;
         do {
